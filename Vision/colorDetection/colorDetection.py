1d06a02a7bab4e22248559698512d027da5cfc76
David P <pegdavid@gmail.com>
2015-10-26 15:14:38 -0400

    Generalized the color detection, cropped image dispayling not working

--- Vision/colorDetection/colorDetection.py
+++ Vision/colorDetection/colorDetection.py
@@ -2,87 +2,89 @@ import cv2
 import numpy
 
 
-def colorDetection(image):
-    #Converts image HSV type image
+def colorDetection(image, lowerBound, upperBound, erosionValue = 15):
+    #Converts image to HSV type image
     hsvImage = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-    testImage = image
+    testImage = image   #ONLY FOR TEST PURPOSES
+
     #Ranges for color detection
-    lowerYellow = numpy.array([20, 100, 100])
-    upperYellow = numpy.array([30,255, 255])
-    lowerBlue = numpy.array([85,100,100])
-    upperBlue = numpy.array([124,255,255])
-    lowerRed = numpy.array([0,100,100])
-    upperRed = numpy.array([19,255,255])
+    lower = numpy.array(lowerBound)
+    upper = numpy.array(upperBound)
 
-    #Ranges applied to the hsvImage
-    yMask = cv2.inRange(hsvImage, lowerYellow, upperYellow)
-    bMask = cv2.inRange(hsvImage, lowerBlue, upperBlue)
-    rMask = cv2.inRange(hsvImage, lowerRed, upperRed)
+    #Ranges applied to hsvImage
+    mask = cv2.inRange(hsvImage, lower, upper)
 
-    #Finding the contours on the image
-    yContours, yHierarchy = cv2.findContours(yMask, cv2.cv.CV_RETR_TREE,
-                                cv2.cv.CV_CHAIN_APPROX_SIMPLE)
-    bContours, bHierarchy = cv2.findContours(bMask, cv2.cv.CV_RETR_TREE,
-                                cv2.cv.CV_CHAIN_APPROX_SIMPLE)
-    rContours, rHierarchy = cv2.findContours(rMask, cv2.cv.CV_RETR_TREE,
-                                cv2.cv.CV_CHAIN_APPROX_SIMPLE)
+    #Remove some noise
+    kernel = numpy.ones((5,5),numpy.uint8)
+    noiseFilter1 = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernel)
+    noiseFilter2 = cv2.morphologyEx(noiseFilter1, cv2.MORPH_CLOSE, kernel)
+    cv2.imshow('ColorMask', noiseFilter2)  #ONLY FOR TEST PURPOSES
 
+    #Finding the contours of the image
+    contours = cv2.findContours(noiseFilter2, cv2.cv.CV_RETR_TREE, cv2.cv.CV_CHAIN_APPROX_SIMPLE)[0]
+
+    (rows, cols) = testImage.shape
+    croppedImages = []
+    centroids = []
     #Given at least one yellow contour
-    if len(yContours) > 0:
-        # Find the index of the largest contour
-        yAreas = [cv2.contourArea(i) for i in yContours]
-        yMaxIndex = numpy.argmax(yAreas)
-        yCnt = yContours[yMaxIndex]
-        #Find coordinate for boundary rectangle
-        yx,yy,yw,yh = cv2.boundingRect(yCnt)
-        #Draw rectangle
-        cv2.rectangle(testImage,(yx-15,yy-15),(yx+yw+15,yy+yh+15),(0,255,255),0)
+    if len(contours) > 0:
+        #creates array of measurements from contours
+        locations = [cv2.boundingRect(cnt) for cnt in contours]
+        #Find the index of the largest contour
+        for (x,y,w,h) in locations:
+            for cnt in contours:
+                if cv2.contourArea(cnt) > 20:
+                    M = cv2.moments(cnt)
+                    cx = int(M['m10']/M['m00'])
+                    cy = int(M['m01']/M['m00'])
+                    centroid = (cx, cy)
+                    cv2.circle(testImage, centroid, 5, (0,0,255), -1)
+                    centroids.append(centroid)
+                    rectX = x - erosionValue
+                    rectY = y - erosionValue
+                    rectW = w + erosionValue
+                    rectH = h + erosionValue
+                    print rectX
+                    print rectY
+                    print rectW
+                    print rectH
+                    if rectX > 0 and rectY > 0 and rectW < rows and rectH < cols:
+                        cImage = testImage[rectX:rectY,rectW:rectH]
+                        cImage = testImage[rectY:rectH, rectX:rectW]
+                    cv2.imshow("TEST", testImage)
 
-    #Given at least one blue contour
-    if len(bContours) > 0:
-        # Find the index of the largest contour
-        bAreas = [cv2.contourArea(i) for i in bContours]
-        bMaxIndex = numpy.argmax(bAreas)
-        bCnt = bContours[bMaxIndex]
-        #Find coordinate for boundary rectangle
-        bx,by,bw,bh = cv2.boundingRect(bCnt)
-        #Draw rectangle
-        cv2.rectangle(testImage,(bx-15,by-15),(bx+bw+15,by+bh+15),(255,0,0),0)
+                        #cv2.imshow("TestWindow", cImage)
+                    cv2.waitKey(8)
 
-    #Given at least one red contour
-    if len(rContours) > 0:
-        # Find the index of the largest contour
-        rAreas = [cv2.contourArea(i) for i in rContours]
-        rMaxIndex = numpy.argmax(rAreas)
-        rCnt = rContours[rMaxIndex]
-        #Find coordinate for boundary rectangle
-        rx,ry,rw,rh = cv2.boundingRect(rCnt)
-        #Draw rectangle
-        cv2.rectangle(testImage,(rx-15,ry-15),(rx+rw+15,ry+rh+15),(0,0,255),0)
+                    # croppedImages.append(cImage)
 
 
-    # #Displaying the masks individually and the final image
-    # cv2.imshow('Yellow Mask', yMask)
-    # cv2.imshow('Blue Mask', bMask)
-    # cv2.imshow('Red Mask', rMask)
-    # cv2.imshow('Altered image', testImage)
-    return testImage
+    return croppedImages, centroids, testImage
 
 def main():
     #Default Camera (cv2.videocapture(-1) the parameter indexes your cameras)
-
-
     camera = cv2.VideoCapture(-1)
-
     while camera.isOpened():
         _, image = camera.read()
         cv2.imshow('Original', image)
+        lowerBlue = numpy.array([85,100,100])
+        upperBlue = numpy.array([124,255,255])
+        #image = cv2.imread("/home/chris/Desktop/opencvtest/TopView.JPG")
+        rectImg, centroids, circleImg = colorDetection(image, lowerBlue, upperBlue)
 
-        rectImg = colorDetection(image)
-        cv2.imshow('Color Detector', rectImg)
-        cv2.waitKey(5)
+        # cv2.circle(rectImg[0],centroids[0], 5, (0,0,255), -1)
+        # cv2.rectangle(rectImg[0],(x-15,y-15),(x+w+15,y+h+15),(0,0,0),0)
 
+        cv2.imshow('Color Detector', circleImg)
 
+        #break loop when ESC is pressed
+        #cv2.waitKey(0)
+        k = cv2.waitKey(5)
+        if k == 27:
+            break
+
+    #close all windows to end program
+    cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
