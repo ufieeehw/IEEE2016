04525b8b31dc7608efc7b378e000c58253f4fc78
Matthew Langford <matthew.langford95@gmail.com>
2015-12-31 16:58:06 -0500

    UDEV: Updated auto run scripts and readme

---
+++ udev/setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cp $DIR/98-ftdi.rules /etc/udev/rules.d/98-ftdi.rules
+cp $DIR/47-hokuyo.rules /etc/udev/rules.d/98-ftdi.rules
+udevadm control --reload-rules
+udevadm trigger
