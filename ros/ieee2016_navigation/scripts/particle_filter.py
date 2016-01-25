96b50e5fc07c2e2794bf61cde2f44a56a41c71a3
Matthew Langford <matthew.langford95@gmail.com>
2016-01-25 18:34:01 -0500

    P_F: Staring to implement GPU acceleration for determining particle weights.

--- ros/ieee2016_navigation/scripts/particle_filter.py
+++ ros/ieee2016_navigation/scripts/particle_filter.py
@@ -12,8 +12,36 @@ import time
 import random
 import matplotlib.pyplot as plt
 from threading import Thread
+import os
+
+import pyopencl as cl
+
 
 PARTICLE_COUNT = 25
+
+class GPUAccMap():
+    def __init__(self):
+        self.map = np.array([      0,     0,     0,2.1336,
+                               .4572,  .762, .4572, 1.143,
+                                .508,     0,  .508, .3048,
+                                .762,     0,  .762, .3048,
+                              1.8669, .8382,1.8669,2.1336,
+                              2.1336,     0,2.1336, .8382,
+                                   0,     0,2.1336,     0,
+                              1.8669, .8382,2.1336, .8382,
+                                .508, .3048,  .762, .3048,
+                                   0,2.1336,1.8669,2.1336]).astype(np.float32)
+
+        # Set up pyopencl
+        self.ctx = cl.create_some_context()
+        self.queue = cl.CommandQueue(self.ctx)
+        self.mf = cl.mem_flags
+        # Load .cl program
+        filename = os.path.join(os.path.dirname(__file__), 'particle_filter.cl')
+        f = open(filename, 'r')
+        fstr = "".join(f.readlines())
+        cl.Program(self.ctx, fstr).build()
+
 
 class ModelMap():
     def __init__(self):
@@ -188,6 +216,205 @@ class Particle():
         )
         return p
 
+class GPUAccFilter(Thread):
+    def __init__(self, p_count, center, radius, heading_range, mW):
+        # Pass the max number of particles, the center and radius of where inital particle generation will be (meters), and the range of heading values (min,max)
+        # ROS Inits
+        self.test_points_pub = rospy.Publisher('/test_points', PoseArray, queue_size=2)
+        self.odom_sub = rospy.Subscriber('/robot/odometry/filtered', Odometry, self.got_odom)
+        self.twist_sub = rospy.Subscriber('/test/test_twist', TwistStamped, self.got_twist)
+        self.laser_scan_sub = rospy.Subscriber('/sim_scan10', LaserScan, self.got_laserscan)
+        self.pose_est_pub = rospy.Publisher('pose_est', PoseStamped, queue_size=2)
+
+        self.m = 
+
+        # We start at arbitrary point 0,0,0
+        self.pose = np.array([.2,.2,1.57], np.float64)
+        self.pose_update = np.array([0,0,0], np.float64) 
+
+        # Generate random point in circle and add to list
+        self.particles = [] 
+        for p in range(p_count):
+            # random angle
+            alpha = 2 * math.pi * random.random()
+            # random radius
+            r = radius * random.random()
+            # calculating coordinates
+            x = r * math.cos(alpha) + center[0]
+            y = r * math.sin(alpha) + center[1]
+
+            # Generate random heading
+            heading = random.uniform(heading_range[0], heading_range[1])
+
+            self.particles.append(Particle(x,y,heading))
+
+        self.laser_scan = np.array([])
+
+        # For keeping track of time
+        self.prev_time = time.time()
+
+        self.hz_counter = 0
+        r = rospy.Rate(10) # 10hz
+        while not rospy.is_shutdown():
+            r.sleep()
+            self.hz_counter = time.time()
+            self.run_filter()
+            print 1.0/(time.time()-self.hz_counter)
+        # self.hz_counter = time.time()
+        # #for i in range(p_count):
+        # self.m.simulate_scan(1.22,1.22,0)
+        # print 1.0/(time.time()-self.hz_counter)
+
+    def run_filter(self):
+        # This is where the filter does its work
+
+        # Check our updated position from the last run of the filter, if it is 0 or close to it, then break and dont worry about running the filter
+        # tolerance = 1e-4
+        # if abs(self.pose_update[0]) < tolerance and\
+        #    abs(self.pose_update[1]) < tolerance and\
+        #    abs(self.pose_update[2]) < tolerance: return
+        while len(self.laser_scan) == 0:
+            print "No scan"
+        update = np.copy(self.pose_update)
+        laser_scan = np.copy(self.laser_scan)[self.m.ranges_to_compare]
+        # Reset the pose update so that the next run will contain the pose update from this point
+        self.pose_update = np.array([0,0,0], np.float64)
+
+        for i,p in enumerate(self.particles):
+            p.update_pos(update)
+            particle_scan = self.m.simulate_scan((p.x,p.y),p.heading,i)
+            p.w = self.compare_with_scan(particle_scan,laser_scan)
+            if p.w > .9: print i,
+        self.publish_particle_array()
+    
+    def compare_with_scan(self,measured_scan,sim_scan):
+        # Found this method online
+        # More accurate values => 1, less accurate => 0
+        sigma2 = .3 ** 2
+        error = measured_scan - sim_scan
+        g = np.mean(math.e ** -(error ** 2 / (2 * sigma2)))
+
+        return g
+
+    def got_twist(self,msg):
+        # Just a temp method to test the filter
+        vehicle_twist = msg.twist
+        
+        time_since_last_msg = self.prev_time - time.time() #seconds
+        self.prev_time = time.time()
+        incoming_msg_freq = 1.0#/time_since_last_msg
+
+        # This accounts for Shia's rotation - if he is pointed at a 45 degree angle and moves straight forward (which is what the twist message will say),
+        # he is not moving directly along the x axis, he is moving at an offset angle.
+        c, s = np.cos(self.pose[2]), np.sin(self.pose[2])
+        rot_mat = np.matrix([
+            [c,     -s],
+            [s,      c],
+        ], dtype=np.float32)
+        # Then we add the x or y translation that we move, rounding down if it's super small
+        x, y = np.dot(rot_mat, [vehicle_twist.linear.x/incoming_msg_freq, vehicle_twist.linear.y/incoming_msg_freq]).A1
+        tolerance = 1e-7
+        if abs(x) < tolerance: x = 0
+        if abs(y) < tolerance: y = 0
+        if abs(vehicle_twist.angular.z) < tolerance: vehicle_twist.angular.z = 0
+        
+        # By summing these components, we get an integral - converting velocity to position
+        self.pose_update += [x, y, vehicle_twist.angular.z/incoming_msg_freq]
+        self.pose += [x, y, vehicle_twist.angular.z/incoming_msg_freq]
+
+        #print "POSE UPDATED"
+
+        q = tf.transformations.quaternion_from_euler(0, 0, self.pose[2])
+        self.pose_est_pub.publish(
+            PoseStamped(
+                header=Header(
+                    stamp=rospy.Time.now(),
+                    frame_id="odom"
+                ),
+                pose=Pose(
+                    position=Point(
+                        x=self.pose[0],
+                        y=self.pose[1],
+                        z=0
+                    ),
+                    orientation=Quaternion(
+                        x=q[0],
+                        y=q[1],
+                        z=q[2],
+                        w=q[3],
+                    )
+                )
+            )
+
+        )
+
+    def got_odom(self,msg):
+        # Update current pose based on odom data, note that this is only an estimation of Shia's position
+        # The twist is a measure of velocity from the previous state
+
+        vehicle_twist = msg.twist.twist
+        incoming_msg_freq = 100 #hz
+
+        # This accounts for Shia's rotation - if he is pointed at a 45 degree angle and moves straight forward (which is what the twist message will say),
+        # he is not moving directly along the x axis, he is moving at an offset angle.
+        c, s = np.cos(self.pose[2]), np.sin(self.pose[2])
+        rot_mat = np.matrix([
+            [c,     -s],
+            [s,      c],
+        ], dtype=np.float32)
+        # Then we add the x or y translation that we move, rounding down if it's super small
+        x, y = np.dot(rot_mat, [vehicle_twist.linear.x/incoming_msg_freq, vehicle_twist.linear.y/incoming_msg_freq]).A1
+        tolerance = 1e-7
+        if abs(x) < tolerance: x = 0
+        if abs(y) < tolerance: y = 0
+        if abs(vehicle_twist.angular.z) < tolerance: vehicle_twist.angular.z = 0
+        
+        # By summing these components, we get an integral - converting velocity to position
+        self.pose_update += [x, y, vehicle_twist.angular.z/incoming_msg_freq]
+        self.pose += [x, y, vehicle_twist.angular.z/incoming_msg_freq]
+
+        q = tf.transformations.quaternion_from_euler(0, 0, self.pose[2])
+        self.pose_est_pub.publish(
+            PoseStamped(
+                header=Header(
+                    stamp=rospy.Time.now(),
+                    frame_id="odom"
+                ),
+                pose=Pose(
+                    position=Point(
+                        x=self.pose[0],
+                        y=self.pose[1],
+                        z=0
+                    ),
+                    orientation=Quaternion(
+                        x=q[0],
+                        y=q[1],
+                        z=q[2],
+                        w=q[3],
+                    )
+                )
+            )
+
+        )
+
+    def got_laserscan(self,msg):
+        self.laser_scan = np.array(msg.ranges)
+
+    def publish_particle_array(self):
+        pose_arr = []
+        for p in self.particles:
+            pose_arr.append(p.return_pose())
+            
+        self.test_points_pub.publish(PoseArray(
+            header=Header(
+                    stamp=rospy.Time.now(),
+                    frame_id="odom",
+                ),
+            poses=pose_arr,
+            )
+        )
+        #print "PUBLISHED PARTICLES"
+
 class Filter(Thread):
     def __init__(self, p_count, center, radius, heading_range, m):
         # Pass the max number of particles, the center and radius of where inital particle generation will be (meters), and the range of heading values (min,max)
@@ -236,7 +463,6 @@ class Filter(Thread):
         # #for i in range(p_count):
         # self.m.simulate_scan(1.22,1.22,0)
         # print 1.0/(time.time()-self.hz_counter)
-
 
     def run_filter(self):
         # This is where the filter does its work
@@ -473,11 +699,11 @@ class Test(Thread):
 
 
 rospy.init_node('particle_filter', anonymous=True)
-m = ModelMap()
-t = Test(m)
-f = Filter(PARTICLE_COUNT, (.21,.23), .15, (1.3,1.8), m)
-t.start()
-f.start()
+# m = ModelMap()
+# t = Test(m)
+# f = Filter(PARTICLE_COUNT, (.21,.23), .15, (1.3,1.8), m,m2)
+# t.start()
+# f.start()
 
 rospy.spin()
 
