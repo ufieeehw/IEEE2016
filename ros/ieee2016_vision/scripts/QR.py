46b6da5c5ae02a16f0d83b9f7d30f0eecf7a7d95
Matthew Langford <matthew.langford95@gmail.com>
2016-01-29 23:03:56 -0500

    VISION: It now returns points coorisponding to each QR code instead of just distance

--- ros/ieee2016_vision/scripts/QR.py
+++ ros/ieee2016_vision/scripts/QR.py
@@ -6,7 +6,7 @@ import numpy as np
 import time
 
 import rospy
-from sensor_msgs.msg import Image as Image_msg, CameraInfo
+from sensor_msgs.msg import Image as Image_msg, CameraInfo, CompressedImage
 from cv_bridge import CvBridge, CvBridgeError
 
 class DetectQRCode(object):
@@ -22,9 +22,9 @@ class DetectQRCode(object):
     """
     def __init__(self, qr_code_count, timeout, image_topic):
         # ROS inits for image updating
-        rospy.init_node('QR_caller', anonymous=True)
-        self.image_sub = rospy.Subscriber(image_topic,Image_msg,self.image_recieved)
-        self.cam_info_sub = rospy.Subscriber(image_topic[:5] + "/camera_info",CameraInfo,self.save_camera_info)
+        #rospy.init_node('QR_caller', anonymous=True)
+        self.image_sub = rospy.Subscriber("/" + image_topic + "/image_raw",Image_msg,self.image_recieved)
+        self.cam_info_sub = rospy.Subscriber("/" + image_topic + "/camera_info",CameraInfo,self.save_camera_info)
 
         self.qr_code_count_max = qr_code_count
         self.timeout = timeout
@@ -38,7 +38,7 @@ class DetectQRCode(object):
         self.frame_id = []
     
     def image_recieved(self,msg):
-        print "Frame updated"
+        #print "Frame updated"
         try:
             self.image = CvBridge().imgmsg_to_cv2(msg, "bgr8")
         except CvBridgeError as e:
@@ -51,68 +51,6 @@ class DetectQRCode(object):
 
         self.camera_info_received = True
         self.cam_info_sub.unregister()
-
-    def test_shit(self):
-        def nothing(self):
-            pass
-        while self.image is 0:
-            print "No image found. Waiting for image."
-            time.sleep(1)
-
-        image = self.image
-        
-        bars_img = np.zeros((300,512,3), np.uint8)
-        cv2.namedWindow('bars')
-        cv2.createTrackbar('blockSize','bars',0,35,nothing)
-        cv2.createTrackbar('C','bars',0,35,nothing)
-        cv2.createTrackbar('erode','bars',10,50,nothing)
-        
-        if image.size > 2: image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-        orig_img = image
-        while not rospy.is_shutdown():
-            image = orig_img
-
-            blockSize = cv2.getTrackbarPos('blockSize','bars')
-            C = cv2.getTrackbarPos('C','bars')
-            erode = cv2.getTrackbarPos('erode','bars')
-            try:
-                #image = cv2.GaussianBlur(image,(3,3),0)
-                image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,blockSize,C)
-
-                #Create the identity filter, but with the 1 shifted to the right!
-                kernel = np.zeros((5,5), np.float32)
-                kernel[4,4] = 2.0   #Identity, times two! 
-
-                #Create a box filter:
-                boxFilter = np.ones((5,5), np.float32) / 25.0
-
-                #Subtract the two:
-                kernel = kernel - boxFilter
-
-                #Note that we are subject to overflow and underflow here...but I believe that
-                # filter2D clips top and bottom ranges on the output, plus you'd need a
-                # very bright or very dark pixel surrounded by the opposite type.
-                image = cv2.filter2D(image, -1, kernel)
-
-                kernel = np.ones((erode/10.0,erode/10.0),np.uint8)
-                image = cv2.erode(image,kernel)
-                
-                color,coor = self.detect_qr(image)
-                if color == None or coor == None: 
-                    print "nothing found"
-                else:
-                    center = ((coor[0][0] + coor[2][0])/2,
-                              (coor[0][1] + coor[2][1])/2)
-                
-                    cv2.circle(mask, center, 20, 0, -1)
-                    print "found",color,center
-                    cv2.waitKey(100)
-
-                cv2.imshow("img", image)
-            except:
-                print "Invalid Paramters"
-            cv2.imshow("bars", bars_img)
-            cv2.waitKey(1)
 
     def begin_processing(self):
         # Process the class image (with multiple threads?) to find all qr codes. The image can be updated
@@ -136,16 +74,16 @@ class DetectQRCode(object):
                 print "Timeout."
                 break
 
-            # Create local copy of objects image so it doesnt get updated halfway through
+            # Create local copy of the image since this is faster to work with and so it doesnt get overwritten
             image = self.image
-            cv2.imwrite("image.jpg",image)
+            #cv2.imwrite("image.jpg",image)
             """
             Convert to Black and white, blur, threshold, sharpen, erode then apply the mask. 
             (Code modified from online resources)
             """
             if image.size > 2: image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
             #image = cv2.GaussianBlur(image,(3,3),0)
-            image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,7)
+            #image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,15,7)
 
             #Create the identity filter, but with the 1 shifted to the right!
             kernel = np.zeros((5,5), np.float32)
@@ -160,36 +98,36 @@ class DetectQRCode(object):
             #Note that we are subject to overflow and underflow here...but I believe that
             # filter2D clips top and bottom ranges on the output, plus you'd need a
             # very bright or very dark pixel surrounded by the opposite type.
-            image = cv2.filter2D(image, -1, kernel)
+            #image = cv2.filter2D(image, -1, kernel)
 
-            kernel = np.ones((2,2),np.uint8)
+            kernel = np.ones((3,3),np.uint8)
             #image = cv2.erode(image,kernel)
             #image = cv2.dilate(image,kernel)
             
             image = image & mask
 
-            cv2.imshow("img", image)
-            cv2.waitKey(1)
+            #cv2.imshow("img", image)
+            #cv2.waitKey(1)
 
 
             color,coor = self.detect_qr(image)
             if color == None or coor == None: 
-                print "nothing found"
+                #print "nothing found"
                 continue
         
-            print coor
+            #print coor
             center = ((coor[0][0] + coor[2][0])/2,
                       (coor[0][1] + coor[2][1])/2)
 
-            dist = self.convert_to_3d_points(coor[0],coor[2])
+            point = self.convert_to_3d_points(coor[0],coor[2])
 
-            self.detected_codes.append([color,center,dist])
+            self.detected_codes.append([color,point])
             qr_code_count += 1
-
-            print "found",color,center
+            print "CODES FOUND:",qr_code_count
+            #print "found",color,center
 
             # Draw mask around detected QR code as to not repeat
-            cv2.circle(mask, center, 20, 0, -1)
+            cv2.circle(mask, center, 40, 0, -1)
 
         print "Time to complete:", time.time() - start_time
         
@@ -234,20 +172,28 @@ class DetectQRCode(object):
         # Paramters
         QR_side_length = 3.64 #cm
         diagonal = np.sqrt(2)*QR_side_length
-        print self.proj_mat
+        #print self.proj_mat
         proj_mat_pinv = np.linalg.pinv(np.array([self.proj_mat]).reshape((3,4)))
 
         # Genereate projection rays through points 1 and 2
         ray_1 = proj_mat_pinv.dot(np.append(point_1,1).reshape((3,1))).reshape((1,4))[0]
         ray_2 = proj_mat_pinv.dot(np.append(point_2,1).reshape((3,1))).reshape((1,4))[0]
 
-        print ray_1,ray_2
-        print np.dot(ray_1,ray_2)
+        #print ray_1,ray_2
+        #print np.dot(ray_1,ray_2)
 
         # Angle between two rays
         mag_1, mag_2 = np.sqrt(ray_1.dot(ray_1)), np.sqrt(ray_2.dot(ray_2))
         theta = np.arccos(np.dot(ray_1,ray_2)/(mag_1 * mag_2))    
-        print "theta",np.degrees(theta)
-        print "diagonal",diagonal   
+
         dist = (diagonal / 2.0) / (np.arctan(theta / 2.0))
-        return dist
+
+        # Calculate unit vector to center point
+        mid_point = [(point_1[0] + point_2[0])/2,
+                      (point_1[1] + point_2[1])/2]
+
+        vect = proj_mat_pinv.dot(np.append(mid_point,1).reshape((3,1))).reshape((1,4))[0][:3]
+        #print vect,dist
+        point = vect * dist / np.linalg.norm(vect)
+
+        return point
--- ros/ieee2016_vision/scripts/QR_temp_caller.py
+++ ros/ieee2016_vision/scripts/QR_temp_caller.py
@@ -1,33 +1,65 @@
 #!/usr/bin/python
 import rospy
+from sensor_msgs.msg import PointCloud, ChannelFloat32
+from geometry_msgs.msg import Point32,PointStamped
+from std_msgs.msg import Header
+
 import QR
 import cv2
 import numpy as np
 import time
+import struct
 
 
+rospy.init_node('temp')
+point_pub = rospy.Publisher("/arm/waypoints", PointCloud, queue_size=1)
 
-# Make detector object. 12 is the number of QR codes to look for, 15 is the timeout in seconds
-detector = QR.DetectQRCode(12,10, "cam_0/image_raw")
+# Make detector object. 8 is the number of QR codes to look for, 15 is the timeout in seconds
+detector = QR.DetectQRCode(22,30, "cam_0")
 
 #Start processing. The ros node will update the images it searchs for
-detected_frames = detector.begin_processing()
-#detector.test_shit()
+detected_points = detector.begin_processing()
+points = []
+channels = [[],[],[]]
+for p in detected_points:
+    if p[0] == "blue":
+        #rgbs.append("FF0000")#struct.pack('i', 0x0000ff))
+        channels[0].append(0) #R
+        channels[1].append(0) #G
+        channels[2].append(1) #B
+    elif p[0] == "red":
+        #rgbs.append(struct.unpack('f', struct.pack('i', 0xff0000))[0])
+        channels[0].append(1) #R
+        channels[1].append(0) #G
+        channels[2].append(0) #B
+    elif p[0] == "green":
+        #rgbs.append(struct.unpack('f', struct.pack('i', 0x00ff00))[0])
+        channels[0].append(0) #R
+        channels[1].append(1) #G
+        channels[2].append(0) #B
+    elif p[0] == "yellow":
+        #rgbs.append(struct.unpack('f', struct.pack('i', 0xffff00))[0])
+        channels[0].append(1) #R
+        channels[1].append(1) #G
+        channels[2].append(0) #B
 
-
-color = (0,0,0)
-image = detector.image
-
-for i in detected_frames:
-    # BGR
-    if i[0] == 'red': color = (0,0,255)
-    if i[0] == 'blue': color = (255,0,0)
-    if i[0] == 'green': color = (0,255,0)
-    if i[0] == 'yellow': color = (0,255,255)
-
-    cv2.circle(image,i[1],5,color,-1)
-    cv2.putText(image,str(i[2])[:4],i[1], cv2.FONT_HERSHEY_SIMPLEX, .6,0,2)
-
-cv2.imshow('final',image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    points.append(Point32(
+            x=p[1][0]/100.0,
+            y=p[1][1]/100.0,
+            z=p[1][2]/100.0
+        )
+    )
+rgb_channels = [ChannelFloat32(name="r", values=channels[0]),ChannelFloat32(name="g", values=channels[1]),ChannelFloat32(name="b", values=channels[2])]
+r = rospy.Rate(10)
+while not rospy.is_shutdown():
+    print "Publishing points"
+    point_pub.publish(PointCloud(
+            header=Header(
+                stamp=rospy.Time.now(),
+                frame_id="camera"
+                ),
+            points=points,
+            channels=rgb_channels
+        )
+    )
+    r.sleep()
