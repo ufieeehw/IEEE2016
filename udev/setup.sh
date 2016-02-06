d8cc784a97e618a076b406f9a45fe59a270a14f8
Matthew Langford <matthew.langford95@gmail.com>
2016-02-06 14:15:46 -0500

    UDEV: Added rule for the C920 camera.

--- udev/setup.sh
+++ udev/setup.sh
@@ -3,5 +3,6 @@
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/98-ftdi.rules /etc/udev/rules.d/98-ftdi.rules
 cp $DIR/47-hokuyo.rules /etc/udev/rules.d/98-ftdi.rules
+cp $DIR/99-cameras.rules /etc/udev/rules.d/99-cameras.rules
 udevadm control --reload-rules
 udevadm trigger
