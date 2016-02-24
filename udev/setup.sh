e2aa68370102bf56e24574c596b7087a3be52e99
ShiaLaBot <its_ian@me.com>
2016-02-23 22:36:58 -0500

    UDEV: Fixing minor name conflicts.

--- udev/setup.sh
+++ udev/setup.sh
@@ -2,7 +2,7 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cp $DIR/98-ftdi.rules /etc/udev/rules.d/98-ftdi.rules
-cp $DIR/47-hokuyo.rules /etc/udev/rules.d/98-ftdi.rules
+cp $DIR/47-hokuyo.rules /etc/udev/rules.d/47-hokuyo.rules
 cp $DIR/99-cameras.rules /etc/udev/rules.d/99-cameras.rules
 udevadm control --reload-rules
 udevadm trigger
