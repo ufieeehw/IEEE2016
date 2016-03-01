3c796ed8cd968524a64f261d41c3175cb0da526f
Matthew Langford <matthew.langford95@gmail.com>
2016-02-29 22:21:57 -0500

    VISION: Started implementing template based matching for QR codes.

--- ros/ieee2016_vision/scripts/QR.py
+++ ros/ieee2016_vision/scripts/QR.py
@@ -1,15 +1,25 @@
+import rospy
+import rospkg
+from sensor_msgs.msg import Image as Image_msg, CameraInfo, CompressedImage
+from cv_bridge import CvBridge, CvBridgeError
+
+
 import QR
 import zbar
 import Image
 import cv2
 import numpy as np
 import time
+import os
 
-import rospy
-from sensor_msgs.msg import Image as Image_msg, CameraInfo, CompressedImage
-from cv_bridge import CvBridge, CvBridgeError
+rospack = rospkg.RosPack()
+TEMPLATES_FOLDER = os.path.join(rospack.get_path('ieee2016_vision'), 'scripts/templates/')
+QR_DISTANCES = [57]
 
-class DetectQRCode(object):
+def nothing(x):
+    pass
+
+class DetectQRCodeZBar(object):
     """
 
     Given an image to process, a max amount of codes to detect and a timeout (in seconds),
@@ -20,61 +30,42 @@ class DetectQRCode(object):
     This relies on the zbar python package.
 
     """
-    def __init__(self, qr_code_count, timeout, image_topic):
+    def __init__(self, qr_code_count, timeout, camera):
         # ROS inits for image updating
         #rospy.init_node('QR_caller', anonymous=True)
-        self.image_sub = rospy.Subscriber("/" + image_topic + "/image_raw",Image_msg,self.image_recieved)
-        self.cam_info_sub = rospy.Subscriber("/" + image_topic + "/camera_info",CameraInfo,self.save_camera_info)
-
+        self.camera = camera
         self.qr_code_count_max = qr_code_count
         self.timeout = timeout
 
         self.detected_codes = []
-
-        self.image = 0
-
-        self.camera_info_received = False
-        self.proj_mat = []
-        self.frame_id = []
-    
-    def image_recieved(self,msg):
-        #print "Frame updated"
-        try:
-            self.image = CvBridge().imgmsg_to_cv2(msg, "bgr8")
-        except CvBridgeError as e:
-            print e
-
-    def save_camera_info(self,msg):
-        print "msg rec"
-        self.proj_mat = msg.P
-        self.frame_id = msg.header.frame_id
-
-        self.camera_info_received = True
-        self.cam_info_sub.unregister()
+        self.image = camera.image
+        cv2.imwrite("image.jpg",self.image)
+        self.begin_processing()
 
     def begin_processing(self):
         # Process the class image (with multiple threads?) to find all qr codes. The image can be updated
-        
-        start_time = time.time()
-        while self.image is 0:
-            print "No image found. Waiting for image."
-            time.sleep(1)
-            if time.time() - start_time > self.timeout:
-                print "Timeout."
-                return 0
         qr_code_count = 0
 
         # The mask acts to remove already detected QR codes so they arent repeateitly detected
         mask = np.zeros(self.image.shape[:2], dtype=np.uint8)+255
-        cv2.rectangle(mask,(0,0),(1920,540),0,-1)
+        #cv2.rectangle(mask,(0,0),(1080,900),0,-1)
 
+        sliders = np.zeros((10,500,3), np.float32)
+        cv2.namedWindow('sliders')
+
+        cv2.createTrackbar('H_L','sliders',0,255,nothing)
+        cv2.createTrackbar('H_U','sliders',0,255,nothing)
+        cv2.createTrackbar('S_L','sliders',0,255,nothing)
+        cv2.createTrackbar('S_U','sliders',0,255,nothing)
+        cv2.createTrackbar('V_L','sliders',0,255,nothing)
+        cv2.createTrackbar('V_U','sliders',0,255,nothing)
 
         start_time = time.time()
         while qr_code_count < self.qr_code_count_max and not rospy.is_shutdown():
             print time.time() - start_time
-            if time.time() - start_time > self.timeout:
-                print "Timeout."
-                break
+            # if time.time() - start_time > self.timeout:
+            #     print "Timeout."
+            #     break
 
             # Create local copy of the image since this is faster to work with and so it doesnt get overwritten
             image = self.image
@@ -85,31 +76,31 @@ class DetectQRCode(object):
             """
             if image.size > 2: image = cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
             #image = cv2.GaussianBlur(image,(3,3),0)
-            image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,3)
+            #image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY,11,3)
+             # Convert BGR to HSV
+            #hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+            #hsv = cv2.bilateralFilter(hsv,3,75,75)
 
-            #Create the identity filter, but with the 1 shifted to the right!
-            kernel = np.zeros((5,5), np.float32)
-            kernel[4,4] = 2.0   #Identity, times two! 
+            # #cv2.imshow('sliders',sliders)
 
-            #Create a box filter:
-            boxFilter = np.ones((5,5), np.float32) / 25.0
+            #lower = np.array([cv2.getTrackbarPos('H_L','sliders'),cv2.getTrackbarPos('S_L','sliders'),cv2.getTrackbarPos('V_L','sliders')])
+            #upper = np.array([cv2.getTrackbarPos('H_U','sliders'),cv2.getTrackbarPos('S_U','sliders'),cv2.getTrackbarPos('V_U','sliders')])
+            # #lower = np.array([27,0,0])
+            # #upper = np.array([239,53,255])
 
-            #Subtract the two:
-            kernel = kernel - boxFilter
+            #Bitwise-AND mask and original image
+            #remove = cv2.inRange(hsv, lower, upper)
+            #image = cv2.bitwise_and(image,image, mask=remove)
+            #image = np.delete(image, (0,1), axis=2)
+            image = cv2.equalizeHist(image)
+            #image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,cv2.THRESH_BINARY,3,3)
 
-            #Note that we are subject to overflow and underflow here...but I believe that
-            # filter2D clips top and bottom ranges on the output, plus you'd need a
-            # very bright or very dark pixel surrounded by the opposite type.
-            image = cv2.filter2D(image, -1, kernel)
 
-            kernel = np.ones((3,3),np.uint8)
-            #image = cv2.erode(image,kernel)
-            #image = cv2.dilate(image,kernel)
-            #print image
             image = image & mask
 
-            cv2.imshow("img", cv2.resize(image, (0,0), fx=0.5, fy=0.5) ); cv2.waitKey(1)
-
+            # Display image
+            cv2.imshow('sliders',sliders)
+            cv2.imshow("img", cv2.resize(image, (0,0), fx=0.8, fy=0.8)); cv2.waitKey(100)
 
             color,coor = self.detect_qr(image)
             if color == None or coor == None: 
@@ -120,9 +111,9 @@ class DetectQRCode(object):
             center = ((coor[0][0] + coor[2][0])/2,
                       (coor[0][1] + coor[2][1])/2)
 
-            point = self.convert_to_3d_points(coor[0],coor[2])
+            #point = self.convert_to_3d_points(coor[0],coor[2])
 
-            self.detected_codes.append([color,point])
+            #self.detected_codes.append([color,point])
             qr_code_count += 1
             print "CODES FOUND:",qr_code_count, color
             #print "found",color,center
@@ -131,8 +122,7 @@ class DetectQRCode(object):
             cv2.circle(mask, center, 20, 0, -1)
 
         print "Time to complete:", time.time() - start_time
-        
-        self.image_sub.unregister()
+    
         return self.detected_codes
 
     def detect_qr(self, image):
@@ -194,4 +184,181 @@ class DetectQRCode(object):
         #print vect,dist
         point = vect * dist / np.linalg.norm(vect)
 
-        return point
+        return poin
+
+class DetectQRCodeTemplateMethod(object):
+    def __init__(self, qr_code_count, camera):
+        # ROS inits for image updating
+        #rospy.init_node('QR_caller', anonymous=True)
+        self.camera = camera
+        self.qr_code_count_max = qr_code_count
+        #self.timeout = timeout
+
+        # Template Lists
+        self.blues = []
+        self.reds = []
+        self.greens = []
+        self.yellows = []
+
+        self.detected_codes = []
+
+        self.load_templates(57)
+        self.match_templates()
+
+
+    def load_templates(self,dist):
+        # Find the closest distance folder (the folders are in cm)
+        self.base_path = os.path.join(TEMPLATES_FOLDER, str(dist)) #str(min(QR_DISTANCES, key=lambda x:abs(x-dist))))
+
+        colors = {"/blue":self.blues,"/red":self.reds,"/green":self.greens,"/yellow":self.yellows}
+        thetas = [0,90,180,270]
+
+        # Save raw image
+        cv2.imwrite(self.base_path+"/raw.jpg",self.camera.image)
+
+        # Check if there is already a folder with rotated and scaled images in it 
+        if cv2.imread(self.base_path+"/blue/0.jpg") is None:
+            # No folder exists so lets make some
+            # Check to make sure template images are there (we assume if the blue one is there, they all are).
+            if cv2.imread(self.base_path+"/blue.jpg") is None:
+                # No folder exists so lets make some
+                print "Template images are missing!"
+                exit()
+
+            print "Rotated templates do not exist. Creating them now."
+            self.make_rotated_templates(colors,thetas)
+
+        for color in colors:
+            for theta in thetas:
+                colors[color].append(cv2.imread(self.base_path+color+"/"+str(theta)+".jpg",0))
+
+
+    def make_rotated_templates(self, colors, thetas):
+        for color in colors:
+            # Make missing folders
+            if not os.path.exists(self.base_path+color):
+                os.makedirs(self.base_path+color)
+
+            # Load template and rotate its
+            template = cv2.imread(self.base_path+color+".jpg",0)
+            rows,cols = template.shape
+
+            # Do some filtering on the template image
+            blur = cv2.GaussianBlur(template,(5,5),2)
+            template = cv2.addWeighted(template,1.5,blur,-.5,0)
+            #template = cv2.equalizeHist(template)
+            for theta in thetas:
+                rot_mat = cv2.getRotationMatrix2D((cols/2,rows/2),theta,1)
+                cv2.imwrite(self.base_path+color+"/"+str(theta)+".jpg",cv2.warpAffine(template,rot_mat,(cols,rows)))
+
+    def match_templates(self):
+        threshold = .65
+        group_distance = 20
+        
+        blues = self.blues 
+        reds = self.reds 
+        greens = self.greens 
+        yellows = self.yellows 
+
+
+        frame_count = 0
+        while not rospy.is_shutdown():
+            found = 0
+            image = self.camera.image
+            gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+            gray = cv2.equalizeHist(gray)
+            
+            existing_points = []
+
+            mid_point = self.blues[0].shape[::-1]
+
+            if frame_count == 0:
+                for template in self.blues:
+                    # Apply matching threshold for this rotation/scale
+                    mid_point = template.shape[::-1]
+                    res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+                    loc = np.where( res >= threshold)
+                    for pt in zip(*loc[::-1]):
+                        # Check if the found point already exists. This should be optimized and made to use an average of centers rather 
+                        # then just the first one it finds.
+                        exists = False
+                        # for existing_point in existing_points:
+                        #     if np.linalg.norm(np.array(existing_point) - np.array(pt)) < group_distance:
+                        #         exists = True
+                        #         break
+                        
+                        if not exists:
+                            found+=1
+                            existing_points.append(pt)
+                            cv2.circle(image, (pt[0] + mid_point[0]/2,pt[1] + mid_point[1]/2), 15, (255,0,0), -1)
+                    #print existing_points
+            elif frame_count == 1:
+                existing_points = []
+                for template in self.reds:
+                    # Apply matching threshold for this rotation/scale
+                    #mid_point = template.shape[::-1]
+                    res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+                    loc = np.where( res >= threshold)
+                    for pt in zip(*loc[::-1]):
+                        # Check if the found point already exists. This should be optimized and made to use an average of centers rather 
+                        # then just the first one it finds.
+                        exists = False
+                        # for existing_point in existing_points:
+                        #     if np.linalg.norm(np.array(existing_point) - np.array(pt)) < group_distance:
+                        #         exists = True
+                        #         break
+                        
+                        if not exists:
+                            found+=1
+                            existing_points.append(pt)
+                            cv2.circle(image, (pt[0] + mid_point[0]/2,pt[1] + mid_point[1]/2), 15, (0,0,255), -1)
+                    #print existing_points
+            elif frame_count == 2:
+                existing_points = []
+                for template in self.greens:
+                    # Apply matching threshold for this rotation/scale
+                    #mid_point = template.shape[::-1]
+                    res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+                    loc = np.where( res >= threshold)
+                    for pt in zip(*loc[::-1]):
+                        # Check if the found point already exists. This should be optimized and made to use an average of centers rather 
+                        # then just the first one it finds.
+                        exists = False
+                        # for existing_point in existing_points:
+                        #     if np.linalg.norm(np.array(existing_point) - np.array(pt)) < group_distance:
+                        #         exists = True
+                        #         break
+                        
+                        if not exists:
+                            found+=1
+                            existing_points.append(pt)
+                            cv2.circle(image, (pt[0] + mid_point[0]/2,pt[1] + mid_point[1]/2), 15, (0,255,0), -1)
+                    #print existing_points
+                    existing_points = []
+
+            elif frame_count == 3:
+                for template in self.yellows:
+                    # Apply matching threshold for this rotation/scale
+                    #mid_point = template.shape[::-1]
+                    res = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+                    loc = np.where( res >= threshold)
+                    for pt in zip(*loc[::-1]):
+                        # Check if the found point already exists. This should be optimized and made to use an average of centers rather 
+                        # then just the first one it finds.
+                        exists = False
+                        # for existing_point in existing_points:
+                        #     if np.linalg.norm(np.array(existing_point) - np.array(pt)) < group_distance:
+                        #         exists = True
+                        #         break
+                        
+                        if not exists:
+                            found+=1
+                            existing_points.append(pt)
+                            cv2.circle(image, (pt[0] + mid_point[0]/2,pt[1] + mid_point[1]/2), 15, (0,255,255), -1)
+                    #print existing_point
+
+            frame_count += 1
+            if frame_count > len(self.blues): frame_count = 0
+            print frame_count
+            cv2.imshow("found",image)
+            cv2.waitKey(1)
--- ros/ieee2016_vision/scripts/QR_temp_caller.py
+++ ros/ieee2016_vision/scripts/QR_temp_caller.py
@@ -4,6 +4,8 @@ from sensor_msgs.msg import PointCloud, ChannelFloat32
 from geometry_msgs.msg import Point32,PointStamped
 from std_msgs.msg import Header
 
+from camera_manager import Camera
+
 import QR
 import cv2
 import numpy as np
@@ -12,54 +14,56 @@ import struct
 
 
 rospy.init_node('temp')
-point_pub = rospy.Publisher("/arm/waypoints", PointCloud, queue_size=1)
+#point_pub = rospy.Publisher("/arm/waypoints", PointCloud, queue_size=1)
+c = Camera("cam_1")
+c.activate()
 
 # Make detector object. 8 is the number of QR codes to look for, 15 is the timeout in seconds
-detector = QR.DetectQRCode(2,10, "cam_0")
+detector = QR.DetectQRCodeTemplateMethod(18, c)
 
-#Start processing. The ros node will update the images it searchs for
-detected_points = detector.begin_processing()
-points = []
-channels = [[],[],[]]
-for p in detected_points:
-    if p[0] == "blue":
-        #rgbs.append("FF0000")#struct.pack('i', 0x0000ff))
-        channels[0].append(0) #R
-        channels[1].append(0) #G
-        channels[2].append(1) #B
-    elif p[0] == "red":
-        #rgbs.append(struct.unpack('f', struct.pack('i', 0xff0000))[0])
-        channels[0].append(1) #R
-        channels[1].append(0) #G
-        channels[2].append(0) #B
-    elif p[0] == "green":
-        #rgbs.append(struct.unpack('f', struct.pack('i', 0x00ff00))[0])
-        channels[0].append(0) #R
-        channels[1].append(1) #G
-        channels[2].append(0) #B
-    elif p[0] == "yellow":
-        #rgbs.append(struct.unpack('f', struct.pack('i', 0xffff00))[0])
-        channels[0].append(1) #R
-        channels[1].append(1) #G
-        channels[2].append(0) #B
+# #Start processing. The ros node will update the images it searchs for
+# detected_points = detector.begin_processing()
+# points = []
+# channels = [[],[],[]]
+# for p in detected_points:
+#     if p[0] == "blue":
+#         #rgbs.append("FF0000")#struct.pack('i', 0x0000ff))
+#         channels[0].append(0) #R
+#         channels[1].append(0) #G
+#         channels[2].append(1) #B
+#     elif p[0] == "red":
+#         #rgbs.append(struct.unpack('f', struct.pack('i', 0xff0000))[0])
+#         channels[0].append(1) #R
+#         channels[1].append(0) #G
+#         channels[2].append(0) #B
+#     elif p[0] == "green":
+#         #rgbs.append(struct.unpack('f', struct.pack('i', 0x00ff00))[0])
+#         channels[0].append(0) #R
+#         channels[1].append(1) #G
+#         channels[2].append(0) #B
+#     elif p[0] == "yellow":
+#         #rgbs.append(struct.unpack('f', struct.pack('i', 0xffff00))[0])
+#         channels[0].append(1) #R
+#         channels[1].append(1) #G
+#         channels[2].append(0) #B
 
-    points.append(Point32(
-            x=p[1][0]/100.0,
-            y=p[1][1]/100.0,
-            z=p[1][2]/100.0
-        )
-    )
-rgb_channels = [ChannelFloat32(name="r", values=channels[0]),ChannelFloat32(name="g", values=channels[1]),ChannelFloat32(name="b", values=channels[2])]
-r = rospy.Rate(10)
-while not rospy.is_shutdown():
-    print "Publishing points"
-    point_pub.publish(PointCloud(
-            header=Header(
-                stamp=rospy.Time.now(),
-                frame_id="camera"
-                ),
-            points=points,
-            channels=rgb_channels
-        )
-    )
-    r.sleep()
+#     points.append(Point32(
+#             x=p[1][0]/100.0,
+#             y=p[1][1]/100.0,
+#             z=p[1][2]/100.0
+#         )
+#     )
+# rgb_channels = [ChannelFloat32(name="r", values=channels[0]),ChannelFloat32(name="g", values=channels[1]),ChannelFloat32(name="b", values=channels[2])]
+# r = rospy.Rate(10)
+# while not rospy.is_shutdown():
+#     print "Publishing points"
+#     point_pub.publish(PointCloud(
+#             header=Header(
+#                 stamp=rospy.Time.now(),
+#                 frame_id="camera"
+#                 ),
+#             points=points,
+#             channels=rgb_channels
+#         )
+#     )
+#     r.sleep()
