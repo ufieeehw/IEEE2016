6bda21a04b3703408d90d54b2fd370a7b18fefed
Anthony Olive <anthony@iris-systems.net>
2016-03-07 18:21:21 -0500

    VISION: Broke up ColorProcessing and laid out Calibration and ColorBox.

--- ros/ieee2016_vision/scripts/color_detection.py
+++ ros/ieee2016_vision/scripts/color_detection.py
@@ -1,39 +1,56 @@
 #!/usr/bin/python2
 
+import json
+
 from camera_manager import Camera
 import cv2
 import numpy as np
+from point_intersector import PointIntersector
 import rospy
 
 
-# These are the upper and lower boundaries for each color
-# These WILL be replaced by an automatically generated calibration file
-RED_HUE = (np.array([0, 100, 100]), np.array([20, 255, 255]))
-GREEN_HUE = (np.array([50, 42, 42]), np.array([80, 255, 255]))
-BLUE_HUE = (np.array([80, 100, 100]), np.array([150, 255, 255]))
-YELLOW_HUE = (np.array([20, 110, 110]), np.array([50, 255, 255]))
-HUE_RANGES = {"red": RED_HUE, "green": GREEN_HUE, "blue": BLUE_HUE, "yellow": YELLOW_HUE}
+class Calibration():
+	'''
+	Manages the calibrations for image color extraction and distance
+	calculation. These are stored in a file in the same directory as the
+	script.
+	'''
+	def __init__(self):
+		self.load_calibration()
+
+	def save_calibration(self):
+		'''
+		Saves the current calibrations to the JSON file
+		'color_calibrations.json' in the same directory as this script.
+		'''
+		pass
+
+	def load_calibration(self):
+		'''
+		Loads the calibrations from the JSON file 'color_calibrations.json' in
+		the same directory as this script and stores them in this class object.
+		'''
+		# These are the upper and lower boundaries for each color
+		# These WILL be replaced by an automatically generated calibration file
+		self.red = (np.array([0, 100, 100]), np.array([20, 255, 255]))
+		self.green = (np.array([50, 42, 42]), np.array([80, 255, 255]))
+		self.blue = (np.array([80, 100, 100]), np.array([150, 255, 255]))
+		self.yellow = (np.array([20, 110, 110]), np.array([50, 255, 255]))
 
 
-class ColorProcessing():
+class Image():
 	'''
-	Contains various color operations that are in the structure of a chain
-	(e.g. top level methods call the methods that they depend on). Some methods
-	require the selection of a color to proceed. Colors id's are as follows:
-	red, green, blue, and yellow. Current top level methods are as follows:
-	resize_frame, select_largest_object, average_box, and get_box_center.
+	Contains various custom image manipulation methods. Some methods require
+	the selection of a color to proceed. Colors id's are as follows: red,
+	green, blue, and yellow.
 	'''
-	def __init__(self, camera):
+	def __init__(self, camera, width = 640):
 		# Baseline operating parameters
-		self.operating_image_width = 320
-		self.image_color_depth = 16
-
 		self.camera = camera
 		self.frame = self.camera.image
-		self.box = None
-		self.box_center = None
+		self.operating_image_width = width
 
-	def resize_frame(self):
+	def resize(self):
 		'''
 		Resizes a frame from the video feed to the operating image with and
 		proportional height.
@@ -44,12 +61,12 @@ class ColorProcessing():
 		new_dimensions = (self.operating_image_width, int(self.operating_image_width / aspect_ratio))
 		self.frame = cv2.resize(self.frame, new_dimensions, interpolation = cv2.INTER_AREA)
 
-	def reduce_colors(self):
+	def reduce_colors(self, image_color_depth):
 		'''
 		Reduces the range of colors present in a frame of the video feed based on
 		the set color depth.
 		'''
-		self.resize_frame()
+		self.resize()
 
 		# Converts the frame to a format that is usable with K-Means clustering
 		working_frame = self.frame.reshape((-1, 3))
@@ -57,7 +74,7 @@ class ColorProcessing():
 
 		# Use K-Means clustering to identify the 16 most predominant colors
 		criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-		ret, label, center = cv2.kmeans(working_frame, self.image_color_depth, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+		ret, label, center = cv2.kmeans(working_frame, image_color_depth, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 		# Reduces the colors in the original image based on the clustering results
 		center = np.uint8(center)
@@ -67,17 +84,30 @@ class ColorProcessing():
 	 	# Blurs the remaining colors to reduce noise
 	 	self.frame = cv2.GaussianBlur(working_frame, (5, 5), 0)
 
-	def color_extraction(self, color):
+	def extract_color(self, color):
 		'''
 		Generates an image from the passed frame that contain hues specified within
 		the defined boundaries for the color id passed.
 		'''
-		self.reduce_colors()
+		self.reduce_colors(16)
 
 		# Creates a mask for the selected hue range and overlays them onto the frame
 		working_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2HSV)
-		mask = cv2.inRange(working_frame, HUE_RANGES[color][0], HUE_RANGES[color][1])
+		mask = cv2.inRange(working_frame, color[0], color[1])
 		self.frame = cv2.bitwise_and(working_frame, working_frame, mask = mask)
+
+
+class ObjectDetection():
+	'''
+	Generates usable data about the location of objects within the frame.
+	Colors id's are as follows: red, green, blue, and yellow.
+	'''
+	def __init__(self, camera):
+		# Baseline operating parameters
+		self.image = Image(camera)
+		self.image.operating_image_width = 320
+		self.box = None
+		self.box_center = None
 
 	def select_largest_object(self, color):
 		'''
@@ -86,10 +116,10 @@ class ColorProcessing():
 		'''
 		largest_area = None
 		largest_contour_index = None
-		self.color_extraction(color)
+		self.image.extract_color(color)
 
 		# Generates a gray frame for the passed color id
-	 	working_frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+	 	working_frame = cv2.cvtColor(self.image.frame, cv2.COLOR_BGR2GRAY)
 
 		# Finds all contours within the frame
 		ret, thresh = cv2.threshold(working_frame, 50, 255, 0)
@@ -165,26 +195,69 @@ class ColorProcessing():
 			self.box_center = None
 
 
+class ColorBox():
+	'''
+	Used to perform the locations and order of the color drop boxes at the
+	beginning of a run. Calculates a 3D point on the map for each box as well
+	as attempting to double check the distance to it.
+	'''
+	def __init__(self, camera):
+			self.detection = ObjectDetection(camera)
+			self.point = PointIntersector()
+			self.colors = ("red", "green", "blue", "yellow")
+
+	def get_map_location(self, color):
+		'''
+		Uses point_intersector to determine the position of a box on the
+		robot's map relative to it's position in the camera's plane.
+		'''
+		pass
+
+	def verify_distance(self, color):
+		'''
+		Calculates the distance to a box based on it's size in the frame. If it
+		is not within an acceptable range of the value obtained from
+		point_intersector, the two values are averaged.
+		'''
+		pass
+
+	def set_waypoint(self, color):
+		'''
+		Creates new waypoints for each of the boxes by color based on the
+		position values obtained by the object.
+		'''
+		pass
+
+	def repeat_for_colors(self, function):
+		'''
+		Repeats the specified function for each of the colors set in the
+		initialization of the class object.
+		'''
+		pass
+
+
 if __name__ == "__main__":
 	'''
 	Serves as a testing platform for the script, displaying the various stages
 	of each processed frame.
 	'''
 	rospy.init_node("color_detection")
-	camera = Camera("cam_1")
+	camera = Camera(1)
 	camera.activate()
-	processing = ColorProcessing(camera)
+	image = Image(camera, 320)
+	detection = ObjectDetection(camera)
+	calibration = Calibration()
 
 	while (True):
 		# The actual processing of the image
-		processing.average_box(processing.select_largest_object, "blue", 8)
-		processing.get_box_center()
+		detection.average_box(detection.select_largest_object, calibration.yellow, 8)
+		detection.get_box_center()
 
 		# Pulling values used to render the debugging image
-		processing.resize_frame()
-		box = processing.box
-		box_center = processing.box_center
-		frame = processing.frame
+		image.resize()
+		frame = image.frame
+		box = detection.box
+		box_center = detection.box_center
 
 		# Draw a box around the selected object in the captured frame
 		if (box):
