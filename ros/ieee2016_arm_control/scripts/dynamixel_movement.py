227648fdb33f8f7ceedf0a4bf7714165b308c000
Matthew Langford <matthew.langford95@gmail.com>
2016-03-31 00:39:07 -0400

    ARM: Added debug prints.

--- ros/ieee2016_arm_control/scripts/dynamixel_movement.py
+++ ros/ieee2016_arm_control/scripts/dynamixel_movement.py
@@ -6,14 +6,11 @@ from dynamixel_io import DynamixelIO
 
 class DynamixelController():
 	def __init__(self):
-		#self.d = DynamixelIO('/dev/ttyUSB3', 1000000)
+		self.d = DynamixelIO('/dev/ttyUSB3', 1000000)
 		self.position = []
-		#self.position[0] = self.d.get_position(1)
-		#self.position[1] = self.d.get_position(2)
-		#self.position[2] = self.d.get_position(3)
-		self.position.append(3)
-		self.position.append(2)
-		self.position.append(1)
+		self.position.append(self.d.get_position(1))
+		self.position.append(self.d.get_position(2))
+		self.position.append(self.d.get_position(3))
 
 	#function that gets position, set as zero point, set multi, pass index & desired position & radius (m)
 	def set_dynamixels(self, servo_ids, position, radius):
@@ -27,8 +24,8 @@ class DynamixelController():
 			if final_position < 0:
 				final_position = 65536 + final_position
 			servo_id.append((index, int(round(final_position))))
-		print servo_id
-		#d.set_multi_position((1, final_position), (2, final_position), (3, final_position))
+		print "SETTING:" servo_id
+		d.set_multi_position((1, final_position), (2, final_position), (3, final_position))
 
 	#state of where it's at, how far it rotated
 	def dynamixel_state(self, servo_ids, radius):
@@ -50,8 +47,9 @@ class DynamixelController():
 			servo_id.append((index, int(round(distance_rotated))))
 			#position in meepy meters
 			distance_meters = distance_rotated * (0.088 * ((2*3.1415926535)/360))
-		print servo_id
-		print distance_meters
+		print "SERVO:",servo_id
+		print "DISTANCE:",distance_meters
+        return distance_meters
 
 d = DynamixelController()
 d.set_dynamixels([1, 2], .006, 1)
